59ca03ff240de41e83916d95d3b337d61ea020ab
chisomije92 <chisomije92@gmail.com>
2023-09-14 08:24:55 +0100

    revise creating users table

--- 2. create-users-table.sql
+++ 2. create-users-table.sql
@@ -1,7 +1,12 @@
+CREATE TYPE enum AS ENUM('employed', 'unemployed', 'self-employed');
 
 CREATE TABLE users (
   full_name VARCHAR(200),
   salary INT,
-  current_status ENUM('employed', 'unemployed', 'self-employed')
+  current_status enum
+  -- current_status ENUM('employed', 'unemployed', 'self-employed') - MYSQL syntax
  
 )
+-- for MYSQL, current_status is created as  " current_status ENUM('employed', 'unemployed', 'self-employed')"
+-- ENUM is not recognized as a type in POSTGRES, hence why we needed to create a type for it here insteads
+
