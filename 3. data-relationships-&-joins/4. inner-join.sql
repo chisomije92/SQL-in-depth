0e53711923aadf1e1efe75cffc92d22817e16336
chisomije92 <chisomije92@gmail.com>
2023-09-18 07:37:59 +0100

    combine multiple joins

--- 3. data-relationships-&-joins/4. inner-join.sql
+++ 3. data-relationships-&-joins/4. inner-join.sql
@@ -1,5 +1,6 @@
 -- SELECT * FROM users
 -- INNER JOIN addresses ON users.address_id = addresses.id; 
 
-SELECT u.id, u.first_name, u.last_name, a.street, a.house_number, a.city_id FROM users AS u
-INNER JOIN addresses AS a ON u.address_id = a.id; 
+SELECT u.id, u.first_name, u.last_name, a.street, a.house_number, c.name AS city_name, a.city_id FROM users AS u
+INNER JOIN addresses AS a ON u.address_id = a.id
+INNER JOIN city AS c ON a.city_id = c.id; 
