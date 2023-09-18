5228640aa4cfc4756fba4279bc6b5d81a1a6d09e
chisomije92 <chisomije92@gmail.com>
2023-09-18 07:34:11 +0100

    basic inner join

---
+++ 3. data-relationships-&-joins/4. inner-join.sql
@@ -0,0 +1,5 @@
+-- SELECT * FROM users
+-- INNER JOIN addresses ON users.address_id = addresses.id; 
+
+SELECT u.id, u.first_name, u.last_name, a.street, a.house_number, a.city_id FROM users AS u
+INNER JOIN addresses AS a ON u.address_id = a.id; 
