3951ad97e3cc7a09861580b32995da575eaaef5a
chisomije92 <chisomije92@gmail.com>
2023-09-18 07:55:30 +0100

    joining and filtering data

---
+++ 3. data-relationships-&-joins/5. inner-join-filter.sql
@@ -0,0 +1,4 @@
+SELECT u.id, u.first_name, u.last_name, a.street, a.house_number, c.name AS city_name, a.city_id FROM users AS u
+INNER JOIN addresses AS a ON u.address_id = a.id
+INNER JOIN city AS c ON a.city_id = c.id
+WHERE c.id = 1 OR c.id = 4; 
