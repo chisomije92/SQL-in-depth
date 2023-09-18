150eb1438aabf7571cd393a028ce760e725465fd
chisomije92 <chisomije92@gmail.com>
2023-09-18 06:49:51 +0100

    create tables for database relations

---
+++ 3. data-relationships-&-joins/2. create-tables.sql
@@ -0,0 +1,22 @@
+CREATE TABLE users (
+    -- id INT PRIMARY KEY AUTO_INCREMENT,
+    Id SERIAL PRIMARY KEY,
+    first_name VARCHAR(300) NOT NULL,
+    last_name VARCHAR(300) NOT NULL,
+    email VARCHAR(300) NOT NULL,
+    address_id INT NOT NULL
+);
+CREATE TABLE addresses (
+    -- id INT PRIMARY KEY AUTO_INCREMENT,
+    Id SERIAL PRIMARY KEY,
+    street VARCHAR(300) NOT NULL,
+    house_number VARCHAR(50) NOT NULL,
+    city_id INT NOT NULL
+);
+
+CREATE TABLE city (
+    -- id INT PRIMARY KEY AUTO_INCREMENT,
+    Id SERIAL PRIMARY KEY,
+    name VARCHAR(300) NOT NULL
+);
+
