ca9fb015e78c3b9842d8e394c1903643f0e64776
chisomije92 <chisomije92@gmail.com>
2023-09-18 12:35:09 +0100

    insert data for relationships

---
+++ 3. data-relationships-&-joins/2. data-relationships/2. insert-data.sql
@@ -0,0 +1,3 @@
+INSERT INTO employees (first_name, last_name, birthdate, email) VALUES('Max', 'Schwarz', '1980-05-10', 'max@test.com');
+INSERT INTO intranet_accounts (email, password) VALUES('max@test.com', '44444');
+
