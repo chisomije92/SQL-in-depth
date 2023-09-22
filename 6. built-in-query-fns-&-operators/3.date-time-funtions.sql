c9f3bb0ea4bbdd5e35e6413c4c76bcb5353ccde4
chisomije92 <chisomije92@gmail.com>
2023-09-22 12:53:48 +0100

    implement date time fns

---
+++ 6. built-in-query-fns-&-operators/3.date-time-funtions.sql
@@ -0,0 +1,15 @@
+-- DOW postgreSQL only
+-- SELECT EXTRACT(ISODOW FROM last_checkin), last_checkin
+-- FROM memberships;
+
+-- SELECT WEEKDAY(last_checkin) + 1, last_checkin
+-- FROM memberships;
+
+-- mysql only
+-- SELECT CONVERT(last_checkin, DATE), CONVERT(last_checkin, TIME)
+-- FROM memberships;
+
+
+--postgres
+SELECT last_checkin::TIMESTAMP::DATE, last_checkin::TIMESTAMP::TIME
+FROM memberships;
