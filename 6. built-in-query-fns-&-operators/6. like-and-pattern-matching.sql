b4626ed065cea867ac866ec47c98783fc7b49129
chisomije92 <chisomije92@gmail.com>
2023-09-22 13:34:59 +0100

    implement like and pattern matching

--- 6. built-in-query-fns-&-operators/6. like-and-pattern-matching.sql
+++ 6. built-in-query-fns-&-operators/6. like-and-pattern-matching.sql
@@ -1,9 +1,14 @@
-SELECT first_name LIKE '_o%', first_name
-FROM memberships;
+-- SELECT first_name LIKE '_o%', first_name
+-- FROM memberships;
 
 
-
+-- CASE SENSITIVE
+-- mysql works just like but does not distinguish case sentivity
 -- SELECT first_name LIKE 'Ma%', first_name
+-- FROM memberships;
+
+-- CASE INSENSITIVE
+-- SELECT first_name ILIKE 'ma%', first_name
 -- FROM memberships;
 
 -- SELECT first_name
