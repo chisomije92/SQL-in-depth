d5005fbeee727849b5636269694de8bf23981030
chisomije92 <chisomije92@gmail.com>
2023-09-22 13:20:20 +0100

    implement like and pattern matching

---
+++ 6. built-in-query-fns-&-operators/6. like-and-pattern-matching.sql
@@ -0,0 +1,11 @@
+SELECT first_name LIKE '_o%', first_name
+FROM memberships;
+
+
+
+-- SELECT first_name LIKE 'Ma%', first_name
+-- FROM memberships;
+
+-- SELECT first_name
+-- FROM memberships
+-- WHERE first_name LIKE 'J____';
