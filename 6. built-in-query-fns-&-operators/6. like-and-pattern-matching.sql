d6066f1567f7a02bb7369887fdbc744a756a30f8
chisomije92 <chisomije92@gmail.com>
2023-09-25 14:57:40 +0100

    add note

--- 6. built-in-query-fns-&-operators/6. like-and-pattern-matching.sql
+++ 6. built-in-query-fns-&-operators/6. like-and-pattern-matching.sql
@@ -1,16 +1,19 @@
 -- SELECT first_name LIKE '_o%', first_name
 -- FROM memberships;
 
+-- Like returns a boolean for all the columns
 
 -- CASE SENSITIVE
--- mysql works just like but does not distinguish case sentivity
+-- mysql works just for just Like but does not distinguish case sensitivity
 -- SELECT first_name LIKE 'Ma%', first_name
 -- FROM memberships;
 
 -- CASE INSENSITIVE
+-- only postgres
 -- SELECT first_name ILIKE 'ma%', first_name
 -- FROM memberships;
 
 -- SELECT first_name
 -- FROM memberships
 -- WHERE first_name LIKE 'J____';
+
