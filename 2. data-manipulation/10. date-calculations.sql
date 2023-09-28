dd288b7480b901c29bb77f419df6f78163089ae7
chisomije92 <chisomije92@gmail.com>
2023-09-28 15:14:59 +0100

    revise

--- 2. data-manipulation/10. date-calculations.sql
+++ 2. data-manipulation/10. date-calculations.sql
@@ -5,7 +5,7 @@ SELECT * FROM sales
 WHERE date_fulfilled - date_created <= 5;
 
 
--- The Extract function below is useful where the date_fulfilled and date_created values are store in timestamp format.
+-- The Extract function below is useful where the date_fulfilled and date_created values are stored in timestamp format.
 -- The Extract function will convert the date_created and date_created values to days 
 -- SELECT * FROM sales
 -- WHERE EXTRACT(DAY FROM date_fulfilled - date_created) <= 5;
